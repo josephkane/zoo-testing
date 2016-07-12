571e0c2997c7b2133c81a61ace1a9f2b9310aeeb
Joseph Kane <josephckane@gmail.com>
2016-07-12 14:47:38 -0500

    refactor animal creation test

--- zoo/test_zoo.py
+++ zoo/test_zoo.py
@@ -7,13 +7,11 @@ class TestAnimal(unittest.TestCase):
 	def setUpClass(self):
 		pass
 
-	def test_name_empty_string_by_default(self):
-		a_animal = zoo.Animal()
-		self.assertEqual(a_animal.name, '')
-
-	def test_species_none_by_default(self):
-		a_animal = zoo.Animal()
-		self.assertEqual(a_animal.species, None)
+	def test_animal_creation(self):
+		bob = zoo.Betta('orange', 'Bob')
+		self.assertEqual(bob.name, 'Bob')
+		self.assertEqual(bob.species.color, 'orange')
+		self.assertIsInstance(bob.species, zoo.BettaTrifasciata)
 
 class TestSpecies(unittest.TestCase):
 
@@ -34,6 +32,43 @@ class TestHabitat(unittest.TestCase):
 	def test_members_empty_set_by_default(self):
 		habitat = zoo.Habitat()
 		self.assertIsInstance(habitat.members, set)
+
+class TestWalking(unittest.TestCase):
+
+	def test_legs_zero_by_default(self):
+		walking = zoo.Walking()
+		self.assertEqual(walking.legs, 0)
+
+	def test_walk_speed_zero_by_default(self):
+		walking = zoo.Walking()
+		self.assertEqual(walking.walk_speed, 0)
+
+class TestSwimming(unittest.TestCase):
+
+	def test_appendages_false_by_default(self):
+		swimming = zoo.Swimming()
+		self.assertFalse(swimming.fins)
+		self.assertFalse(swimming.flippers)
+		self.assertFalse(swimming.web_feet)
+
+	def test_swim_speed_zero_by_default(self):
+		swimming = zoo.Swimming()
+		self.assertEqual(swimming.swim_speed, 0)
+
+class TestFlying(unittest.TestCase):
+
+	def test_wings_zero_by_default(self):
+		flying = zoo.Flying()
+		self.assertFalse(flying.wings, 0)
+
+	def test_wingspan_zero_by_default(self):
+		flying = zoo.Flying()
+		self.assertFalse(flying.wingspan, 0)
+
+	def test_air_speed_zero_by_default(self):
+		flying = zoo.Flying()
+		self.assertEqual(flying.air_speed, 0)
+
 
 if __name__ == '__main__':
 	unittest.main()
--- zoo/zoo.py
+++ zoo/zoo.py
@@ -1,8 +1,8 @@
 class Animal:
 
-	def __init__(self):
-		self.name = ''
-		self.species = None
+	def __init__(self, name, species):
+		self.name = name
+		self.species = species
 
 class Species:
 
@@ -10,8 +10,41 @@ class Species:
 		self.common_name = ''
 		self.geo_region = ''
 
+class BettaTrifasciata(Species):
+
+	def __init__(self, color):
+		self.color = color
+		self.common_name = 'Betta'
+		self.geo_region = 'Asia'
+
+class Betta(Animal):
+	def __init__(self, color, name):
+		Animal.__init__(self, name, BettaTrifasciata(color))
+
 class Habitat:
 
 	def __init__(self):
 		self.name = ''
 		self.members = set()
+
+class Walking:
+
+	def __init__(self):
+		self.legs = 0
+		self.walk_speed = 0
+
+
+class Swimming:
+
+	def __init__(self):
+		self.fins = False
+		self.flippers = False
+		self.web_feet = False
+		self.swim_speed = 0
+
+class Flying:
+
+	def __init__(self):
+		self.wings = 0
+		self.wingspan = 0
+		self.air_speed = 0
